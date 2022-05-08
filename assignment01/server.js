496eb11240694fdea014bca07f77970fced94499
ALEXIE NARCISO <NA.LEXIE.AN@gmail.com>
2022-05-08 16:45:36 -0700

    modified server.js to  work with profile.ejs

--- assignment01/server.js
+++ assignment01/server.js
@@ -9,20 +9,13 @@
 // }***
 
 
-
-
-
-
-
-
-
 const express = require('express')
 const app = express()
 app.set('view engine', 'ejs');
 
 app.listen(4000, function (err) {
     if (err) console.log(err);
-})   
+})
 
 // app.get('/', function (req, res) {
 //     res.send('<h1> GET request to homepages pt2 </h1>')    
@@ -34,16 +27,39 @@ app.listen(4000, function (err) {
 
 app.use(express.static('./public'))
 
+const https = require('https');
+
 app.get('/profile/:id', function (req, res) {
     // res.send(`<h1> Hey the id is: ${req.params.id} </h1>`)   
-    
-    res.render("profile.ejs",{
-        "id": req.params.id,
+
+    const url = `https://pokeapi.co/api/v2/pokemon/${req.params.id}`
+
+    https.get(url, function (https_res) {
+        data = '';
+        https_res.on("data", function (chunk) {
+            // console.log(JSON.parse(chunk));
+            data += chunk
+        })
+
+        https_res.on('end', function () {
+            data = JSON.parse(data)
+            //     console.log(data)
+            // console.log(JSON.parse(data));
+            // console.log("data" + JSON.parse(data));
+
+            hp_ = data.stats.filter((obj_) => {
+                return obj_.stat.name == "hp"
+            }).map((obj2) => {
+                return obj2.base_stat
+            });
+            console.log(hp_)
+            res.render("profile.ejs", {
+                "id": req.params.id,
+                "name": data.name,
+                "img_path": '', //data.sprites.other["official-artwork"]["front_default"],
+                "hp": hp_[0]
+            });
+        })
     });
 
-    // res.json({
-    //     "k1":"v1",
-    //     "k2":"v1",
-    //     "k3":"v1",
-    // })
 })
