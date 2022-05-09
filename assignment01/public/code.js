54a97790d3efc8d164e6efe66d7c97ab250c721c
ALEXIE NARCISO <NA.LEXIE.AN@gmail.com>
2022-05-08 23:37:50 -0700

    add more to display cards in code.js

--- assignment01/public/code.js
+++ assignment01/public/code.js
@@ -1,21 +1,33 @@
 
 to_add = ""
 
-function processPokeResp(data) {
+function processPokeResp(data){
     // console.log(data)
 
     //3- etract the image and process the respones
-    to_add += `${data.name} <div class="image_container">
+    to_add += `<div class="pokemon_information"> 
+    <div class="image_container">
     <a href="/profile/${data.id}"> 
     <img src="${data.sprites.other["official-artwork"].front_default}"> 
     </a>
+    </div>
+    <div class="info>
+    <span class="number"># ${data.id}</span>
+    <h2> ${data.name}<h2>
+    <div class="type">
+    Type: 
+    <span>
+    ${data.type}
+    </span>
+    </div>
+    </div>
     </div>`
 }
 
 async function loadNineImages() {
     for (i = 1; i <= 9; i++) {// loops Nine times
         if (i % 3 == 1) {// if index is 1,4,7... create new div
-            to_add += `<div class="images_group">`
+            to_add += `<div class="images_group">`;
         }
 
         // 1 - generater random numbers
@@ -32,7 +44,6 @@ async function loadNineImages() {
             to_add += `</div> `;
 
         }
-
 
     }
     // this will add the value 'to_add' from db to main div
